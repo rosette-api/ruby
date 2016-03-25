067488c3f84bb669e9657bd822195af110879b81
Chris Park <cpark@basistech.com>
2016-03-25 14:24:13 -0400

    Docker and script cleanup. Added error detection.

--- examples/docker/run_ruby.sh
+++ examples/docker/run_ruby.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+ping_url="https://api.rosette.com/rest/v1"
+retcode=0
+errors=( "Exception" "processingFailure" )
+
+#------------ Start Functions --------------------------
+
 #Gets called when the user doesn't provide any args
 function HELP {
     echo -e "\nusage: source_file.rb API_KEY [ALT_URL]"
@@ -8,6 +14,56 @@ function HELP {
     echo "  ALT_URL      - Alternate service URL (optional)"
     exit 1
 }
+
+#Checks if Rosette API key is valid
+function checkAPI {
+    match=$(curl "${ping_url}/ping" -H "X-RosetteAPI-Key: ${API_KEY}" |  grep -o "forbidden")
+    if [ ! -z $match ]; then
+        echo -e "\nInvalid Rosette API Key"
+        exit 1
+    fi  
+}
+
+
+# strip the trailing slash off of the alt_url if necessary
+function cleanURL() {
+    if [ ! -z "${ALT_URL}" ]; then
+        case ${ALT_URL} in
+            */) ALT_URL=${ALT_URL::-1}
+                echo "Slash detected"
+                ;;
+        esac
+        ping_url=${ALT_URL}
+    fi
+}
+
+#Checks for valid url
+function validateURL() {
+    match=$(curl "${ping_url}/ping" -H "X-RosetteAPI-Key: ${API_KEY}" |  grep -o "Rosette API")
+    if [ "${match}" = "" ]; then
+        echo -e "\n${ping_url} server not responding\n"
+        exit 1
+    fi  
+}
+
+function runExample() {
+    echo -e "\n---------- ${1} start -------------"
+    result=""
+    if [ -z ${ALT_URL} ]; then
+        result="$(ruby ${1} ${API_KEY} 2>&1 )"
+    else
+        result="$(ruby ${1} ${API_KEY} ${ALT_URL} 2>&1 )"
+    fi
+    echo "${result}"
+    echo -e "\n---------- ${1} end -------------"
+    for err in "${errors[@]}"; do 
+        if [[ ${result} == *"${err}"* ]]; then
+            retcode=1
+        fi
+    done
+}
+
+#------------ End Functions ----------------------------
 
 #Gets API_KEY, FILENAME and ALT_URL if present
 while getopts ":API_KEY:FILENAME:ALT_URL" arg; do
@@ -27,52 +83,26 @@ while getopts ":API_KEY:FILENAME:ALT_URL" arg; do
     esac
 done
 
-ping_url="https://api.rosette.com/rest/v1"
+cleanURL
 
-# strip the trailing slash off of the alt_url if necessary
-if [ ! -z "${ALT_URL}" ]; then
-    case ${ALT_URL} in
-        */) ALT_URL=${ALT_URL::-1}
-            echo "Slash detected"
-            ;;
-    esac
-    ping_url=${ALT_URL}
-fi
-
-#Checks for valid url
-match=$(curl "${ping_url}/ping" -H "X-RosetteAPI-Key: ${API_KEY}" |  grep -o "Rosette API")
-if [ "${match}" = "" ]; then
-    echo -e "\n${ping_url} server not responding\n"
-    exit 1
-fi  
-
-#Checks if Rosette API key is valid
-function checkAPI {
-    match=$(curl "${ping_url}/ping" -H "X-RosetteAPI-Key: ${API_KEY}" |  grep -o "forbidden")
-    if [ ! -z $match ]; then
-        echo -e "\nInvalid Rosette API Key"
-        exit 1
-    fi  
-}
-
+validateURL
 
 #Copy the mounted content in /source to current WORKDIR
-cp /source/*.* .
+cp -r -n /source/. .
 
 #Run the examples
 if [ ! -z ${API_KEY} ]; then
     checkAPI
+    cd examples
     if [ ! -z ${FILENAME} ]; then
-        if [ ! -z ${ALT_URL} ]; then
-	    ruby ${FILENAME} ${API_KEY} ${ALT_URL} 
-	else
-	    ruby ${FILENAME} ${API_KEY} 
-   	fi
-    elif [ ! -z ${ALT_URL} ]; then
-    	find -maxdepth 1  -name '*.rb' -print -exec ruby {} ${API_KEY} ${ALT_URL} \;
+        runExample ${FILENAME}
     else
-	find -maxdepth 1  -name '*.rb' -print -exec ruby {} ${API_KEY} \;
+        for file in *.rb; do
+            runExample ${file}
+        done
     fi
 else 
     HELP
 fi
+
+exit ${retcode}

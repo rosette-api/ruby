493202800ac890d3fe023ceba8f09051ad7da797
Chris Park <cpark@basistech.com>
2016-05-16 14:17:09 -0400

    Small cleanup in relationships example

--- examples/relationships.rb
+++ examples/relationships.rb
@@ -10,9 +10,6 @@ end
 
 relationships_text_data = 'The Ghostbusters movie was filmed in Boston.'
 params = DocumentParameters.new(content: relationships_text_data)
-options = {
-    "accuracyMode" => "PRECISION"
-}
-params.rosette_options = options
+params.rosette_options = { "accuracyMode" => "PRECISION" }
 response = rosette_api.get_relationships(params)
 puts JSON.pretty_generate(response)

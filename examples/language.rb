da042b3a863defb3795a548a7fe6181df8a1c16d
Hannah <hgaudet@basistech.com>
2016-06-29 15:46:26 -0400

    changing custom header

--- examples/language.rb
+++ examples/language.rb
@@ -10,6 +10,6 @@ end
 
 language_data = 'Por favor Señorita, says the man.?'
 params = DocumentParameters.new(content: language_data)
-params.custom_headers = { 'X-RosetteAPI-App': 'ruby-app'}
+params.custom_headers = { 'X-RosetteAPI-App'=> 'ruby-app'}
 response = rosette_api.get_language(params)
 puts JSON.pretty_generate(response)

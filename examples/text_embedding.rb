6e8eff967c9ae6d1817d44cd16fdce10b560ffbc
Chris Park <cpark@basistech.com>
2016-08-29 09:58:38 -0400

    Added text-embedding
    - Added text_embedding.rb example (still requires data tag update)
    - Added unit test - passed
    - Revised unit tests to no longer rely on mock-data
    - Removed mock-data

---
+++ examples/text_embedding.rb
@@ -0,0 +1,14 @@
+require 'rosette_api'
+
+api_key, url = ARGV
+
+if !url
+  rosette_api = RosetteAPI.new(api_key)
+else
+  rosette_api = RosetteAPI.new(api_key, url)
+end
+
+entities_text_data = 'Bill Murray will appear in new Ghostbusters film: Dr. Peter Venkman was spotted filming a cameo in Boston this… http://dlvr.it/BnsFfS'
+params = DocumentParameters.new(content: entities_text_data, genre: 'social-media')
+response = rosette_api.get_text_embedding(params)
+puts JSON.pretty_generate(response)
